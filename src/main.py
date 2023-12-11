32f22a91b2c4c56ec4756ca0603ac5071d28a7ff
stevemberg <stevemberg100@gmail.com>
2023-12-11 03:39:18 -0300

    runner and tester

--- src/main.py
+++ src/main.py
@@ -6,7 +6,8 @@ application for Magnolia usage.
 from argparse import ArgumentParser
 
 from project.project import Project
-
+from runner.runner import Runner
+from tester.tester import Tester
 
 def get_arguments():
     """
@@ -25,6 +26,9 @@ def get_arguments():
     parser.add_argument(
         "--run", "-r", help="Run magnolia over a project", required=False
     )
+    parser.add_argument(
+        "--test", "-t", help="Run all tests of Oberon-Scala", required=False
+    )
 
     return parser.parse_args()
 
@@ -35,10 +39,10 @@ def main():
     """
     arguments = get_arguments()
 
-    if arguments.install and arguments.run:
+    if arguments.install and arguments.run and arguments.test:
         print("ERROR: Two operations at the same time not supported.")
         exit(-1)
-    elif not arguments.install and not arguments.run:
+    elif not arguments.install and not arguments.run and not arguments.test:
         print("ERROR: An operation must be defined.")
 
     project = Project(arguments.project)
@@ -47,6 +51,10 @@ def main():
         print("Install option defined.")
     elif arguments.run:
         print("Run option enabled.")
+        Runner.__init__(project)
+    elif arguments.test:
+        print("Running tests...")
+        Tester.__init__(project)
     else:
         print("Option not defined.")
 
--- src/project/project.py
+++ src/project/project.py
@@ -11,16 +11,26 @@ The Oberon project file must be a json file.
 from pathlib import Path
 import os
 import sys
+import json
 
 PROJECT_FILENAME = ".oberon.json"
 
+def get_dot_oberon():
+    """
+    Get .oberon.json content.
+    """
+    f = open(PROJECT_FILENAME)
+    data = json.load(f)
+    f.close()
+    return data
 
 class Project:
     def __init__(self, project_path):
         project_path = Path(project_path)
-        project_file = project_path / PROJECT_FILENAME
+        self.project_file = project_path / PROJECT_FILENAME
+        self.data = get_dot_oberon()
 
-        if not project_file.is_file():
+        if not self.project_file.is_file():
             print(
                 (
                     "ERROR: Project file not defined, please define "
