f7b1105ca276eaa8a7db23d8713c42a963d37f8a
uventura <ualivent@gmail.com>
2023-12-18 19:42:07 -0300

    Fixing runner message

--- src/runner/runner.py
+++ src/runner/runner.py
@@ -3,6 +3,8 @@ Runner class.
 """
 
 import subprocess
+import os
+import sys
 
 
 class Runner:
@@ -13,6 +15,13 @@ class Runner:
 
     # pylint: disable=too-few-public-methods
     def __init__(self, interpreter, file):
+        if not os.path.isfile(interpreter):
+            print(f"[ERROR] The interpreter `{interpreter}` does not exists.")
+            sys.exit(-1)
+        if not os.path.isfile(file):
+            print(f"The file `{file}` does not exists.")
+            sys.exit(-1)
+
         subprocess.run(
             ["java", "-jar", interpreter, "interpreter", "-i", file], check=True
         )
